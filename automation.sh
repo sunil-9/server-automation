a52af8fc305bb115980c3704c45282512958e16a
Romeo sunil sapkota <sunilsapkota9@gmail.com>
2020-08-25 19:18:34 +0545

    bug fixed and update features

--- automation.sh
+++ automation.sh
@@ -20,6 +20,33 @@ if [[ $EUID -ne 0 ]]; then
     exit 1;
 fi
 
+installServerSetup(){
+    echo -e "${RED}[*] Choose apache in the server list while installing phpmyadmin${NC}"
+    echo -e "${YELLOW}[+] updating System first${NC}"
+    sudo apt-get update -y > /dev/null && sudo apt-get upgrade -y > /dev/null
+    echo -e "${YELLOW}[+] Installing apache2 server and its tools${NC}"
+    sudo apt-get install apache2 apache2-doc apache2-utils libexpat1 ssl-cert -y > /dev/null
+    echo -e "${YELLOW}[+] Installing php and its tools${NC}"
+    sudo apt-get install php libapache2-mod-php zip -y > /dev/null
+    echo -e "${YELLOW}[+] Installing Mysql Database${NC}"
+    sudo apt-get install mysql-server mysql-client -y > /dev/null
+    echo -e "${YELLOW}[+] Installing Phpmyadmin${NC}"
+    sudo apt-get install phpmyadmin -y 
+    echo -e "${YELLOW}[+] Changing permissions${NC}"
+    sudo chown -R www-data:www-data /var/www
+    echo -e "${YELLOW}[+] enable and restarting services${NC}"
+    echo ""
+    sudo service apache2 restart > /dev/null
+    sudo service mysql restart > /dev/null
+    sudo a2enmod rewrite > /dev/null
+    sudo systemctl enable apache2 > /dev/null
+    sudo systemctl enable mysql > /dev/null
+    sudo systemctl restart apache2 > /dev/null
+    sudo systemctl restart mysql > /dev/null
+    echo  -e "${GREEN}[+] Done Click enter to continue${NC}"
+    read
+}
+
 #remove disable swap, remove it and remove entry from fstab
 function removeSwap(){
     echo -e "${YELLOW}[+] Removing swap and backup fstab.${NC}"
@@ -159,10 +186,13 @@ function setupSwapMain() {
 }
 ########### virtual host ######
 virtualHost(){
+        echo -e "${BLUE} You have following sites enabled"
+    sudo ls -l /etc/apache2/sites-enabled/
+    echo -e "${NC}"
   default_domain="example.com"
-  read -p "[?] Enter domain name default name [$default_domain]" name
+  read -p "[?] Enter domain name default name [$default_domain]: " name
   name="${name:-$default_domain}"
-  DEFAULT_WEB_ROOT_DIR="/var/www/$name/public_html/"
+  DEFAULT_WEB_ROOT_DIR="/var/www/$name/public_html"
   read -p "[?] Enter web root default  WEB_ROOT_DIR [$DEFAULT_WEB_ROOT_DIR]: " WEB_ROOT_DIR
     WEB_ROOT_DIR="${WEB_ROOT_DIR:-$DEFAULT_WEB_ROOT_DIR}"
  DEFAULT_EMAIL="admin@$name"
@@ -185,8 +215,9 @@ virtualHost(){
         <VirtualHost *:80>
           ServerAdmin $email
           ServerName $name
+          ServerAlias www.$name
           DocumentRoot $WEB_ROOT_DIR
-          <Directory $WEB_ROOT_DIR/>
+          <Directory $WEB_ROOT_DIR>
             Options Indexes FollowSymLinks
             AllowOverride all
           </Directory>
@@ -204,6 +235,9 @@ virtualHost(){
     read
 }
 virtualHostDelete(){
+    echo -e "${BLUE} You have following sites enabled"
+    sudo ls -l /etc/apache2/sites-enabled/
+    echo -e "${NC}"
     default_domain="example.com"
     read -p "[+] Enter domain name default name [$default_domain]: " name
     name="${name:-$default_domain}"
@@ -234,35 +268,6 @@ virtualHostDelete(){
     read
 
 }
-installServerSetup(){
-    echo -e "${RED}[*] Choose apache in the server list while installing phpmyadmin${NC}"
-    echo -e "${YELLOW}[+] updating System first${NC}"
-    sudo apt-get update -y > /dev/null && sudo apt-get upgrade -y > /dev/null
-    echo -e "${YELLOW}[+] Installing apache2 server and its tools${NC}"
-    sudo apt-get install apache2 apache2-doc apache2-utils libexpat1 ssl-cert -y > /dev/null
-    echo -e "${YELLOW}[+] Installing php and its tools${NC}"
-    sudo apt-get install php libapache2-mod-php -y > /dev/null
-    echo -e "${YELLOW}[+] Installing Mysql Database${NC}"
-    sudo apt-get install mysql-server mysql-client -y > /dev/null
-    echo -e "${YELLOW}[+] Installing Phpmyadmin${NC}"
-    sudo apt-get install phpmyadmin -y 
-    echo -e "${YELLOW}[+] Changing permissions${NC}"
-    sudo chown -R www-data:www-data /var/www
-    echo -e "${YELLOW}[+] enable and restarting services${NC}"
-    echo ""
-    sudo service apache2 restart > /dev/null
-    sudo service mysql restart > /dev/null
-    sudo a2enmod rewrite > /dev/null
-    sudo systemctl enable apache2 > /dev/null
-    sudo systemctl enable mysql > /dev/null
-    sudo systemctl restart apache2 > /dev/null
-    sudo systemctl restart mysql > /dev/null
-    echo  -e "${GREEN}[+] Done Click enter to continue${NC}"
-    read
-
-
-
-}
 phpmyadmin(){ 
     echo "[?] Enter Domain name: "
     read domain
@@ -274,11 +279,11 @@ phpmyadmin(){
     pwd
     echo -e "${YELLOW}[*] Installing Phpmyadmin on $domain please wait it will take a while."
     echo -e "${YELLOW}[*] Installing Composer${NC}"
-   apt-get install composer -y > /dev/null;
+    apt-get install composer -y > /dev/null;
     echo -e "${YELLOW}[*] creating project to install phpmyadmin${NC}"
-  composer create-project phpmyadmin/phpmyadmin > /dev/null;
+    composer create-project phpmyadmin/phpmyadmin > /dev/null;
     echo -e "${YELLOW}[*] Installing Phpmyadmin${NC}"
-  composer create-project phpmyadmin/phpmyadmin --repository-url=https://www.phpmyadmin.net/packages.json --no-dev > /dev/null
+    composer create-project phpmyadmin/phpmyadmin --repository-url=https://www.phpmyadmin.net/packages.json --no-dev > /dev/null
     echo  -e "${GREEN}[+] Done Click enter to continue${NC}"
   read
 }
@@ -292,100 +297,158 @@ fullAutomatedWP(){
     read -p "[+] Enter domain name default name [$default_domain]: " name
     name="${name:-$default_domain}"
      DEFAULT_WEB_ROOT_DIR="/var/www/$name/public_html/"
-  read -p "[+] Enter web root default  WEB_ROOT_DIR [$DEFAULT_WEB_ROOT_DIR]: " WEB_ROOT_DIR
+    read -p "[+] Enter web root default  WEB_ROOT_DIR [$DEFAULT_WEB_ROOT_DIR]: " WEB_ROOT_DIR
     WEB_ROOT_DIR="${WEB_ROOT_DIR:-$DEFAULT_WEB_ROOT_DIR}"
     echo -e "${YELLOW}[+] Deleting web root dir $WEB_ROOT_DIR${NC}"
     rm -r "$WEB_ROOT_DIR"
     echo -e "${YELLOW}[+] Creating web root dir $WEB_ROOT_DIR${NC}"
     mkdir -p "$WEB_ROOT_DIR"
-# read PROJECT_SOURCE_URL
+    echo  -e "${YELLOW}[+] Downloading wordpress${NC}"
+    WORDPRESS_URL="https://wordpress.org/latest.tar.gz"
 
-    # echo $PROJECT_SOURCE_URL;read;
-# Downloading wordpress
-echo  -e "${YELLOW}[+] Downloading wordpress${NC}"
-WORDPRESS_URL="https://wordpress.org/latest.tar.gz"
+    # GET ALL USER INPUT
+    echo "[?] Project folder name?(if any)"
+    read PROJECT_FOLDER_NAME
 
-# GET ALL USER INPUT
-echo "[?] Project folder name?(if any)"
-read PROJECT_FOLDER_NAME
+    echo "[?] Setup wp_config? (y/n)"
+    read SHOULD_SETUP_DB
 
-echo "[?] Setup wp_config? (y/n)"
-read SHOULD_SETUP_DB
+    if [ $SHOULD_SETUP_DB = 'y' ]
+    then
+      echo "[?] Enter DB Name"
+      read DB_NAME
 
-if [ $SHOULD_SETUP_DB = 'y' ]
-then
-  echo "[?] Enter DB Name"
-  read DB_NAME
-
-  echo "[?] Enter DB Username"
-  read DB_USERNAME
+      echo "[?] Enter DB Username"
+      read DB_USERNAME
 
 
-  echo "[?] Enter DB Password"
-  read DB_PASSWORD
-fi
+      echo "[?] Enter DB Password"
+      read DB_PASSWORD
+    fi
 
-#LETS START INSTALLING
-echo  -e "${YELLOW}[+] Sit back and relax :) ......${NC}"
+    #LETS START INSTALLING
+    echo  -e "${YELLOW}[+] Sit back and relax :) ......${NC}"
 
-# CREATE PROJECT DIRECTORIES
-mkdir -p "$PROJECT_SOURCE_URL"
-cd "$WEB_ROOT_DIR"
-echo "${YELLOW}[+] Creating $WEB_ROOT_DIR${NC}"
-mkdir -p "$PROJECT_FOLDER_NAME"
-cd "$PROJECT_FOLDER_NAME"
+    # CREATE PROJECT DIRECTORIES
+    cd "$WEB_ROOT_DIR"
+    echo -e "${YELLOW}[+] Creating $WEB_ROOT_DIR${NC}"
+    mkdir -p "$PROJECT_FOLDER_NAME" > /dev/null
+    cd "$PROJECT_FOLDER_NAME" > /dev/null
 
-# DOWNLOAD WORDPRESS
-echo  -e "${YELLOW}Downloading Wordpress${NC}"
-curl -O $WORDPRESS_URL
+    # DOWNLOAD WORDPRESS
+    echo  -e "${YELLOW}Downloading Wordpress${NC}"
+    curl -O $WORDPRESS_URL
 
-# UNZIP WORDPRESS AND REMOVE ARCHIVE FILES
-echo -e  "${YELLOW}Unzipping Wordpress${NC}"
-tar -xzf latest.tar.gz
-rm -f latest.tar.gz
-cd wordpress/
-mv * ../
-cd ..
-rm -r wordpress
+    # UNZIP WORDPRESS AND REMOVE ARCHIVE FILES
+    echo -e  "${YELLOW}Unzipping Wordpress${NC}"
+    tar -xzf latest.tar.gz
+    rm -f latest.tar.gz
+    mv wordpress/* . 
 
-if [ $SHOULD_SETUP_DB = 'y' ]
-then
-  # SETUP WP CONFIG
-  echo  -e "${YELLOW}[+] Create wp_config${NC}"
-ls
-  mv wp-config-sample.php wp-config.php
-  sed -i "s/^.*DB_NAME.*$/define('DB_NAME', '$DB_NAME');/" wp-config.php
-  sed -i "s/^.*DB_USER.*$/define('DB_USER', '$DB_USERNAME');/" wp-config.php
-  sed -i "s/^.*DB_PASSWORD.*$/define('DB_PASSWORD', '$DB_PASSWORD');/" wp-config.php
-  echo ""
-  echo ""
-   echo -e "${YELLOW}[+] creating database${NC}"
-  MYSQL=`which mysql`
-  Q1="CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-  Q2="GRANT ALL ON *.* TO '$DB_USERNAME'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
-  Q3="FLUSH PRIVILEGES;"
-  SQL="${Q1}${Q2}${Q3}"
-  $MYSQL -uroot -e "$SQL"
-  echo  -e "${Green}Database $DB_NAME and user $DB_USERNAME created with your password.${NC}"
-fi
-echo  -e "${GREEN}[+] All done press enter to continue${NC}"
-read
+    if [ $SHOULD_SETUP_DB = 'y' ]
+    then
+      # SETUP WP CONFIG
+      echo  -e "${YELLOW}[+] Create wp_config${NC}"
+      mv wp-config-sample.php wp-config.php
+      sed -i "s/^.*DB_NAME.*$/define('DB_NAME', '$DB_NAME');/" wp-config.php
+      sed -i "s/^.*DB_USER.*$/define('DB_USER', '$DB_USERNAME');/" wp-config.php
+      sed -i "s/^.*DB_PASSWORD.*$/define('DB_PASSWORD', '$DB_PASSWORD');/" wp-config.php
+      echo ""
+      echo ""
+       echo -e "${YELLOW}[+] creating database${NC}"
+      MYSQL=`which mysql`
+      Q1="CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+      Q2="GRANT ALL ON *.* TO '$DB_USERNAME'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
+      Q3="FLUSH PRIVILEGES;"
+      SQL="${Q1}${Q2}${Q3}"
+      $MYSQL -uroot -e "$SQL"
+      echo  -e "${Green}Database $DB_NAME and user $DB_USERNAME created with your password.${NC}"
+    fi
+      echo  -e "${YELLOW}[+] Manage permissions${NC}"
+      sudo chown -R www-data:www-data .
+    echo  -e "${GREEN}[+] All done press enter to continue${NC}"
+    read
 
 }
-installssl(){
-    echo -e "${CYAN}"
+installSSl(){
+    echo -e "${BLUE}"
+    default_domain="example.com"
+    read -p "[+] Enter domain name default name [$default_domain]: " name
+    name="${name:-$default_domain}"
+    sudo ufw allow https
     sudo add-apt-repository ppa:certbot/certbot
-    sudo apt install python-certbot-apache
-    sudo certbot --apache -d example.com
+    echo  -e "${YELLOW}[+] Installing packages${NC}"
+    sudo apt install python-certbot-apache -y 
+    sudo certbot --apache -d "${name}"
     echo -e "${NC}"
+    echo  -e "${GREEN}[+] All done press enter to continue${NC}"
+    read
 
 }
 
-renewSSL(){
-  chmod a+x certbot-auto
-  cp certbot-auto /etc/letsencrypt/
-  cd /etc/letsencrypt/ && ./certbot-auto renew && /etc/init.d/apache2 restart
-  echo -e "${BLUE} [+] All done press enter to continue ${NC}"
+WPbackup(){
+        echo "Please provide mysql info"
+    MYSQL_HOST="localhost"
+    read -p "[+] Please enter NEW_MYSQL_HOST [$MYSQL_HOST]: " NEW_MYSQL_HOST
+    NEW_MYSQL_HOST="${NEW_MYSQL_HOST:-$MYSQL_HOST}"
+    # echo $NEW_MYSQL_HOST
+
+    MYSQL_PORT="3306"
+    read -p "[+] Please enter NEW_MYSQL_PORT [$MYSQL_PORT]: " NEW_MYSQL_PORT
+    NEW_MYSQL_PORT="${NEW_MYSQL_PORT:-$MYSQL_PORT}"
+    # echo $NEW_MYSQL_PORT
+
+    MYSQL_USER="newuser"
+    read -p "[+] Please enter NEW_MYSQL_USER [$MYSQL_USER]: " NEW_MYSQL_USER
+    NEW_MYSQL_USER="${NEW_MYSQL_USER:-$MYSQL_USER}"
+    # echo $NEW_MYSQL_USER
+
+    MYSQL_PASSWORD="password"
+    read -p "[+] Please enter NEW_MYSQL_PASSWORD [$MYSQL_PASSWORD]: " NEW_MYSQL_PASSWORD
+    NEW_MYSQL_PASSWORD="${NEW_MYSQL_PASSWORD:-$MYSQL_PASSWORD}"
+    # echo $NEW_MYSQL_PASSWORD
+
+    DATABASE_NAME="DB_name"
+    read -p "[+] Please enter NEW_DATABASE_NAME [$DATABASE_NAME]: " NEW_DATABASE_NAME
+    NEW_DATABASE_NAME="${NEW_DATABASE_NAME:-$DATABASE_NAME}"
+    # echo $NEW_DATABASE_NAME
+    echo "you have following domain running"
+    ls -l /etc/apache2/sites-enabled/
+
+    echo " please provide domain name to backup"
+    default_domain="example.com"
+    read -p "[?] Enter domain name default name [$default_domain]: " name
+    name="${name:-$default_domain}"
+    DEFAULT_WEB_ROOT_DIR="/var/www/$name/public_html"
+    read -p "[?] Enter web root default  WEB_ROOT_DIR [$DEFAULT_WEB_ROOT_DIR]: " WEB_ROOT_DIR
+    WEB_ROOT_DIR="${WEB_ROOT_DIR:-$DEFAULT_WEB_ROOT_DIR}"
+
+    NOW=$(date +"%Y-%m-%d-%H%M")
+    FILE="${name}.${NOW}"
+
+    echo "--------------------------------------------------------------------------------"
+    echo "[?] Say yes if you want to remote back up and no if you want local backup? (y/n)"
+    echo "--------------------------------------------------------------------------------"
+    read backup_remote
+
+    if [ $backup_remote = 'y' ]; then
+      echo "[+] This feature is comming soon. Press enter to continue"
+      read
+    else
+        mkdir backup/${name} > /dev/null
+        mkdir backup/${name}-temp > /dev/null
+        mv  backup/${name}/* backup/${name}-temp/ 
+        echo "[+] local Backup started"
+        mysqldump -h ${NEW_MYSQL_HOST} \
+       -P ${NEW_MYSQL_PORT} \
+       -u ${NEW_MYSQL_USER} \
+       -p${NEW_MYSQL_PASSWORD} \
+       ${NEW_DATABASE_NAME} | zip > backup/${name}/${NEW_DATABASE_NAME}-${FILE}.sql.zip
+       zip -r backup/${name}/${FILE}.zip  $WEB_ROOT_DIR/* 
+       rm -r backup/${name}-temp
+    echo  -e "${GREEN}[+] All done press enter to continue${NC}"
+       read
+    fi
 }
 programStart(){
 while true; do 
@@ -403,11 +466,11 @@ while true; do
     echo "[*] Enter 2 to create swap Memory."
     echo "[*] Enter 3 to create virtual host."
     echo "[*] Enter 4 to Delete virtual host."
-    echo "[*] Enter 5 to install phpmyadmin."
+    echo "[*] Enter 5 to install phpmyadmin in a domain."
     echo "[*] Enter 6 to update your system."
     echo "[*] Enter 7 to install wordpress ."
-    echo "[*] Enter 7 to install SSL certificate(to make your website having https)."
-    echo "[*] Enter 7 to renew SSL certificate . (it should be renew every 3 month)"
+    echo "[*] Enter 8 to install OR Renew SSL certificate(To make your website having https)."
+    echo "[*] Enter 9 to backup wordpress"
     echo "[*] Any other number to exit."
     echo "[?] Enter Your choice: "
     read choice
@@ -449,10 +512,8 @@ while true; do
         ;;
 
        9)
-        renewSSL
+        WPbackup
         ;;
-
-
       *)
         echo ""
         echo  "Good Bye !"
